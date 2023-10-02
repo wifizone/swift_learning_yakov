7f5743450494fc7e1a046e65a02077a9f1c31e8e
Полуянов Яков <poluyanov.yasha@MacBook-Pro-Poluanov.local>
2023-10-02 23:21:45 +0500

    empty project

---
+++ CustomCollections/CustomCollections/CustomCollectionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCollectionsApp.swift
+//  CustomCollections
+//
+//  Created by Полуянов Яков on 01.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomCollectionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
